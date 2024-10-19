cba6b9a77654066186331704c40d3d53ee36b496
jasmincervantes490 <jasmin.cervantes490@campus.mccd.edu>
2024-10-18 23:20:39 -0700

    Update ScrabbleGame.java

--- ScrabbleGame.java
+++ ScrabbleGame.java
@@ -1,6 +1,6 @@
 // Jasmin Cervantes
 // Date: 10.17.2024
-// I was not able to record a meeting because my group did not respond, I included the screenshots in Canvas
+// I was not able to record a meeting because my group computer was acting weird and for some reason it did not display my groupmates response, I included the screenshots in Canvas
 // I will turn in my response to the questions in a document that will be submitted to Canvas
 
 
